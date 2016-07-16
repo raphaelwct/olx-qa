b679faa21122e771ce5ef95e9f6dbe8e14ee550e
Raphael Carvalho <raphaelwct@gmail.com>
2016-07-16 16:34:45 -0300

    Implementando step para procurar um item na pagina de anuncios e para verificar se o item procurado foi encontrado.

--- tests/functional/features/steps/search.py
+++ tests/functional/features/steps/search.py
@@ -9,9 +9,13 @@ def access_advertisement_page(context):
 
 @when('I search for "{item}"')
 def search_for_item(context, item):
-    assert True is not False
+    search_field = context.browser.find_element_by_id('searchtext')
+    search_field.send_keys(item)
+    search_button = context.browser.find_element_by_id('searchbutton')
+    search_button.click()
 
 
 @then('I have to find advertisements for "{item}"')
 def check_ads(context, item):
-    assert context.failed is False
+    ads = context.browser.find_elements_by_xpath("//ul[@id='main-ad-list']//li")
+    assert len(ads) > 2
