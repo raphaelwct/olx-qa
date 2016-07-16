76aaf85564a6d2426ff60dfbdd813bcf5c9e0156
Raphael Carvalho <raphaelwct@gmail.com>
2016-07-16 16:55:20 -0300

    Adicionamos teste funcional funcional para garantir que o portal esta comunicando corretamento ao usuario quando um item procurado nao é encontrado em nossa base de anuncios.

--- tests/functional/features/search.feature
+++ tests/functional/features/search.feature
@@ -4,3 +4,9 @@ Feature: Search
      Given I access the advertisement page
       when I search for "apartamento"
       then I have to find advertisements for "apartamento"
+  
+  
+  Scenario: User search for an item and don't find advertisements
+     Given I access the advertisement page
+      when I search for "foobarxpty"
+      then I dont find advertisements
--- tests/functional/features/steps/search.py
+++ tests/functional/features/steps/search.py
@@ -18,4 +18,15 @@ def search_for_item(context, item):
 @then('I have to find advertisements for "{item}"')
 def check_ads(context, item):
     ads = context.browser.find_elements_by_xpath("//ul[@id='main-ad-list']//li")
-    assert len(ads) > 2
+    assert len(ads) >= 1
+
+
+@then('I dont find advertisements')
+def check_ads(context):
+    ads = context.browser.find_elements_by_xpath("//ul[@id='main-ad-list']//li")
+    section_not_found = context.browser.find_element_by_xpath("//div[@class='section_not-found']")
+    not_found_msg = context.browser.find_element_by_xpath("//div[@class='section_not-found']//h3[@class='subtitle mb5px']")
+
+    assert len(ads) == 0
+    assert section_not_found
+    assert not_found_msg.text == u"Nenhum anúncio foi encontrado."
