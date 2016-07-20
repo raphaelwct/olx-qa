501472ac6b4c5f58534c92491284ccf27768c2c8
Raphael Carvalho <raphaelwct@gmail.com>
2016-07-20 00:34:04 -0300

    Test garantindo que a api esta respondendo de acordo com o protocolo de comunicacao definido.

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -5,20 +5,8 @@ def test_post_should_work_properly():
     ads_data = {
         "propertyCode": "763088",
         "propertyType": "Apartamento",
-        "description": "Bairro Botafogo - novo ícone de qualidade, Condomínio Clube com 2650 m². Varanda, 4 dormitórios sendo 1 suite, 2 vagas na escritura. Entrega: novembro/2016.",
-        "updatedAt": "2016-05-23",
-        "address": {
-            "city": "Rio de Janeiro",
-            "neighbourhood": "Botafogo",
-            "number": 161,
-            "complement": "Bl.02",
-            "zipCode": "22271-100"
-        },
-        "photos": [
-            {
-                "url": "http://images2.brbrokers.com.br/CRM/763088/Medium42505.6554280903_d5cf61da1778e511843d02bfc0a80045160515033259fachada.jpg"
-            },
-        ]
+        "description": "Apartamento Novo - Bairro Botafogo ",
+        "updatedAt": "2016-05-23"
     }
     post_api_response = requests.post(
         'http://httpbin.org/post',
@@ -26,3 +14,9 @@ def test_post_should_work_properly():
     )
 
     assert post_api_response.status_code == 200
+    assert 'form' in post_api_response.json()
+    post_api_data = post_api_response.json()['form']
+    assert post_api_data['propertyCode'] == ads_data['propertyCode']
+    assert post_api_data['propertyType'] == ads_data['propertyType']
+    assert post_api_data['description'] == ads_data['description']
+    assert post_api_data['updatedAt'] == ads_data['updatedAt']
