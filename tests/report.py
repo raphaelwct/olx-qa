9def5a7644d89631dc8e8b0498a77404d23dffbf
Raphael Carvalho <raphaelwct@gmail.com>
2016-07-23 12:05:52 -0300

    Criando estrutura inicial para termos um relatorio via web dos testes funcionais.

---
+++ tests/report.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def tests_report():
+    try:
+        tests_results_file = open('functional/tests_result.txt', 'r')
+        str_file_content = tests_results_file.read()
+        function_tests_info = "<br>".join(str_file_content.split("\n"))
+        tests_results_file.close()
+    except IOError:
+        function_tests_info = u"""
+            Não existem ainda resultados da nossa suite de testes funcionais. <br><br>
+            Para obter feedback sobre nossos testes funcionais sigua os passos abaixo: <br>
+            1) Entre no diretório do projeto <br>
+            2) Execute o comando a seguir: <br>
+            behave tests/functional/features/ --format null --summary --multiline > tests/functional/tests_result.txt <br><br>
+            E em seguida acesse novamente essa url para ver o relatório dos testes.<br>
+        """
+    return render_template('tests_report.html', function_tests_info=function_tests_info)
