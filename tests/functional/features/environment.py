e4b1fa543bce37851dfff12a6e1cc80d51042f08
Raphael Carvalho <raphaelwct@gmail.com>
2016-07-23 15:59:37 -0300

    Retirando comentario do enviroment.py

--- tests/functional/features/environment.py
+++ tests/functional/features/environment.py
@@ -25,9 +25,8 @@ def before_all(context):
 def after_all(context):
     context.browser.close()
 
+
 def after_step(context, step):
     if BEHAVE_DEBUG_ON_ERROR and step.status == "failed":
-        # -- ENTER DEBUGGER: Zoom in on failure location.
-        # NOTE: Use IPython debugger, same for pdb (basic python debugger).
         import ipdb
         ipdb.post_mortem(step.exc_traceback)
