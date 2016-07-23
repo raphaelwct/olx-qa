6d2edf8c024eca49622087031aaebe87ea21e4c8
Raphael Carvalho <raphaelwct@gmail.com>
2016-07-23 15:26:36 -0300

    Criando uma view e template separados para o relatorio de testes funcionais.

--- tests/report.py
+++ tests/report.py
@@ -8,10 +8,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def tests_report():
+    return render_template('tests_report.html')
+
+
+@app.route('/funcional-tests-report')
+def functional_tests_report():
     try:
         tests_results_file = open('functional/tests_result.txt', 'r')
         str_file_content = tests_results_file.read()
-        function_tests_info = "<br>".join(str_file_content.split("\n"))
+        functional_tests_info = "<br>".join(str_file_content.split("\n"))
         tests_results_file.close()
     except IOError:
         function_tests_info = u"""
@@ -22,4 +27,4 @@ def tests_report():
             behave tests/functional/features/ --format null --summary --multiline > tests/functional/tests_result.txt <br><br>
             E em seguida acesse novamente essa url para ver o relatório dos testes.<br>
         """
-    return render_template('tests_report.html', function_tests_info=function_tests_info)
+    return render_template('functional_tests_report.html', functional_tests_info=functional_tests_info)
