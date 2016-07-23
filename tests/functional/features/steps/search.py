09309e98c1395e44c03e506cc43a5f1e67f31e65
Raphael Carvalho <raphaelwct@gmail.com>
2016-07-23 16:02:51 -0300

    Organizando melhor arquivo de steps, para que fique mais facil de ler o xpath dos elementos da tela.

--- tests/functional/features/steps/search.py
+++ tests/functional/features/steps/search.py
@@ -17,15 +17,23 @@ def search_for_item(context, item):
 
 @then('I have to find advertisements for "{item}"')
 def check_ads(context, item):
-    ads = context.browser.find_elements_by_xpath("//ul[@id='main-ad-list']//li")
+    ads = context.browser.find_elements_by_xpath(
+        "//ul[@id='main-ad-list']//li"
+    )
     assert len(ads) >= 1
 
 
 @then('I dont find advertisements')
-def check_ads(context):
-    ads = context.browser.find_elements_by_xpath("//ul[@id='main-ad-list']//li")
-    section_not_found = context.browser.find_element_by_xpath("//div[@class='section_not-found']")
-    not_found_msg = context.browser.find_element_by_xpath("//div[@class='section_not-found']//h3[@class='subtitle mb5px']")
+def check_no_ads_behave(context):
+    ads = context.browser.find_elements_by_xpath(
+        "//ul[@id='main-ad-list']//li"
+    )
+    section_not_found = context.browser.find_element_by_xpath(
+        "//div[@class='section_not-found']"
+    )
+    not_found_msg = context.browser.find_element_by_xpath(
+        "//div[@class='section_not-found']//h3[@class='subtitle mb5px']"
+    )
 
     assert len(ads) == 0
     assert section_not_found
