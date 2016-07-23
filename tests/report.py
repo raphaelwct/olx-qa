d88dea3940e1a2ebd77149c12efc707f5ff92d54
Raphael Carvalho <raphaelwct@gmail.com>
2016-07-23 15:48:50 -0300

    Cria uma view para renderizar o resultado dos testes de integracao.

--- tests/report.py
+++ tests/report.py
@@ -11,6 +11,22 @@ def tests_report():
     return render_template('tests_report.html')
 
 
+@app.route('/integration-tests-report')
+def integration_tests_report():
+    try:
+        return render_template('integration_tests_report.html')
+    except Exception:
+        integration_tests_info = u"""
+            Não existem ainda resultados da nossa suite de testes de integração. <br><br>
+            Para obter feedback sobre nossos testes de integração siga os passos abaixo: <br>
+            1) Entre no diretório do projeto <br>
+            2) Execute o comando a seguir: <br>
+            py.test --html=tests/templates/integration_tests_report.html <br><br>
+            E em seguida acesse novamente essa url para ver o relatório dos testes.<br>
+        """
+        return render_template('integration_tests_help.html', integration_tests_info=integration_tests_info)
+
+
 @app.route('/funcional-tests-report')
 def functional_tests_report():
     try:
@@ -19,9 +35,9 @@ def functional_tests_report():
         functional_tests_info = "<br>".join(str_file_content.split("\n"))
         tests_results_file.close()
     except IOError:
-        function_tests_info = u"""
+        functional_tests_info = u"""
             Não existem ainda resultados da nossa suite de testes funcionais. <br><br>
-            Para obter feedback sobre nossos testes funcionais sigua os passos abaixo: <br>
+            Para obter feedback sobre nossos testes funcionais siga os passos abaixo: <br>
             1) Entre no diretório do projeto <br>
             2) Execute o comando a seguir: <br>
             behave tests/functional/features/ --format null --summary --multiline > tests/functional/tests_result.txt <br><br>
