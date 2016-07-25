3023e7a3cdac151f7e55959c977fc25c6965a207
Raphael Carvalho <raphaelwct@gmail.com>
2016-07-25 02:00:48 -0300

    Removendo configuracao de tamanho da janela do browser.

--- tests/functional/features/environment.py
+++ tests/functional/features/environment.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 
 def before_all(context):
     browser = webdriver.PhantomJS()
-    browser.set_window_size(1120, 550)
     browser.implicitly_wait(5)
     context.browser = browser
 
